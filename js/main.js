774584897e801d84c1364bca3de6805a74b19e6f
l-bissell78429 <43183061+l-bissell78429@users.noreply.github.com>
2019-01-23 17:54:33 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// create jar, name jar, place string in jar
+var firstName = "Marco";
+
+// alert is a method(built in function in JS)
+// alert("hello" + firstName);
+
+//modern JS
+// alert(`hello ${firstname}`);
+
+// console.log("hello" + firstName);
+
+console.log("javascript linked up");
